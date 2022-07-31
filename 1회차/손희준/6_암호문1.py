1243f8a3132ee79b999dc1ee17fe87f9fd034ae5
sony-jun <warrise@naver.com>
2022-07-31 13:13:52 +0900

    암호문1확인

--- 1회차/손희준/6_암호문1.py
+++ 1회차/손희준/6_암호문1.py
@@ -1,21 +1,32 @@
-for i in range(10):
-    n = int(input())
-    code_text = list(input().split())
-    co_num = int(input())
-    n_n = []
-    lin = []
+for t in range(1, 11) :
+    N = int(input())
+    code = list(map(int, input().split()))
+    M = int(input())
+    instruction = list(input().split())
 
-# i 숫자 숫자 암호코드~ i 숫자 숫자 암호코드의 반복
-    commend = list(input().split('I '))
-    for j in range(len(commend)):
-        lin.append([commend[j]])
-print(lin)
-        
-# 여기서 어떻게 구성해야 할지 모르겠다....
-# 분명히 ['1 1 111111 111111']이 아닌 [1,1,111111,111111]같이 정수로
-# 인식해야 할 것 같은데 여기서 막힘....
+    ins = []
+    for i in range(len(instruction)) :
+        if instruction[i] == "I" :
+            if i != 0 :
+                ins.append(tmp)
+            tmp = []
+        elif i == len(instruction)-1 :
+            tmp.append(int(instruction[i]))
+            ins.append(tmp)
+        else :
+            tmp.append(int(instruction[i]))
+
+    for i in range(M) :
+        x = ins[i][0]
+        y = ins[i][1]
+        s = ins[i][2:]
+        for j in range(y) :
+            code.insert(x+j, s[j])
+
+    print("#{} {} {} {} {} {} {} {} {} {} {}".format(t, *code))
 
 
-    
+# 답을보고 나서 생각한건데 이 당시에는 내가 못푸는 문제가 아니였나...
+# 실력의 부족을 통감했음
 
 
